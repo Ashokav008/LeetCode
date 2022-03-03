5a1901df4d15221f55791cd1d1229d5aaa07da60
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-03-03 20:08:43 +0530

    Time: 56 ms (89.63%), Space: 21.6 MB (98.91%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     
-    
+    bool ok=true;
     int moves=0;
-    void dfs(int i,int j,int n,int m,vector<vector<int>>&grid){
+    
+    void dfs(int i,int j,int n,int m,int &ans,vector<vector<int>>&grid){
         
-     if(i<0 or j<0 or i>=n or j>=m or grid[i][j]==0)
+          if (i<0 or j<0 or i>=n or j>=m or grid[i][j]==0)
             return;
         
+          else  if((i==0 or j==0 or i==n-1 or j==m-1)){
+            ok=false;
+            return;
+        }
+    
         grid[i][j]=0;
-        moves=moves+1;  
+        ans=ans+1;  
         
-         dfs(i+1,j,n,m,grid);
-          dfs(i,j+1,n,m,grid);
-          dfs(i-1,j,n,m,grid);
-          dfs(i,j-1,n,m,grid);
+          dfs(i+1,j,n,m,ans,grid);
+          dfs(i,j+1,n,m,ans,grid);
+          dfs(i-1,j,n,m,ans,grid);
+          dfs(i,j-1,n,m,ans,grid);
         
     }
     
@@ -22,27 +28,24 @@ public:
         moves=0;
         int n=grid.size();
         int m=grid[0].size();
-      
+      int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 or j==0 or i==n-1 or j==m-1){
+                
                 if(grid[i][j]==1){
-                   dfs(i,j,n,m,grid);   
-                }
+                    ans=0;
+                    dfs(i,j,n,m,ans,grid);
+                    if(ok)
+                        moves+=ans;
+                    
+                    ok=true;
+                    ans=0;
+                    }
                 }
             }
-        }
         
-        moves=0;
-        //finding all middle iselands
-         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                   dfs(i,j,n,m,grid);
-                       
-                }
-            }
-        }
+        
+        
         return moves;
     }
 };
