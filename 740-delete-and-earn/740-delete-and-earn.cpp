1bcfa50bdfecc37ca5c47698f7914a34d016886c
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-03-05 21:01:23 +0530

    Time: 17 ms (31.16%), Space: 18 MB (8.57%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+
+        if(nums.size()==1)
+            return nums[0];
+        
+      
+        
+        
+        int maxi=*max_element(nums.begin(),nums.end());
+        vector<int>freq(20005);
+        vector<int>dp(20005);
+       for(auto i:nums)
+           freq[i]++;
+        
+        dp[1]=freq[1];
+        dp[2]=max(dp[1],freq[2]*2);
+        
+        for(int i=3;i<=maxi;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+freq[i]*i);
+            
+        }
+        
+        return dp[maxi];
+    }
+};
