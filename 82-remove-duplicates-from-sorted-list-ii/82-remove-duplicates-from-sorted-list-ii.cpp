b8735fba8decce920f2b15473ab4a6f9172f5ab9
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-03-09 13:08:20 +0530

    Time: 18 ms (19.58%), Space: 11.2 MB (59.49%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -8,51 +8,37 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-ListNode* deleteDuplicates(ListNode* head) {
-
- // if the no. of nodes are 0 or 1, just return
- if( !head || !(head->next))
-     return head;
- 
- // prev & curr are used for connecting prev with current unique node and newhead is the
- //head of the new modified linked list
- ListNode* prev = NULL, *curr= head, *newhead = nullptr;
- 
- /// while curr node exists
- while( curr){
-
-     if(curr->next && curr->val == curr->next->val){
-         
-         // move to the last same_value node
-         while( curr->next && curr->val == curr->next->val){
-             curr= curr->next;
-         }
-     }
-     
-     
-     else{
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL   )
+            return NULL;
         
-         if( !newhead)
-             newhead= curr;
-     
-         else
-             prev->next = curr;
-            
-         //change the prev_node to the curr_node (it will serve as prev for the next unique node
-         prev = curr;
-     }
-     
-    
-     curr= curr->next;
+        if(!(head->next))
+                return head;
+        ListNode* prev=NULL,*newNode=nullptr ,*curr=head;
+        
+        while(curr){
+        if(curr->next and curr->val==curr->next->val ){
+            while(curr->next and curr->val==curr->next->val){
+                curr=curr->next;
+            }
+        }
+            else{
+                if(!newNode)
+                    newNode=curr;
+                
+                else
+                    prev->next=curr;
 
- }
- if(prev)
+                prev=curr;
+            }
+        
+         curr=curr->next;   
+        }
+        
+    if(prev)
      prev->next= NULL;
- 
- return newhead;
-}
-
+        return newNode;
+    }
 };
