229f5ec07634b6103d29eba1328c5021c144b81f
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-03-15 22:40:44 +0530

    Added solution - LeetHub

---
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        // your code here
+        vector<int>ans;
+        int i;
+        deque<int>dq;
+        for( i=0;i<k;i++){
+            while(!dq.empty() and arr[i]>=arr[dq.back()]) 
+                dq.pop_back();
+                
+            dq.push_back(i);
+        }
+        
+        ans.push_back(arr[dq.front()]);
+        
+        for(; i<n;i++){
+            while(!dq.empty() and dq.front()<=i-k)
+                dq.pop_front();
+                
+            while(!dq.empty() and  arr[i]>=arr[dq.back()])
+                dq.pop_back();
+              dq.push_back(i);
+              ans.push_back(arr[dq.front()]);
+            
+        }
+        return ans;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+        vector<int> res = ob.max_of_subarrays(arr, n, k);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
