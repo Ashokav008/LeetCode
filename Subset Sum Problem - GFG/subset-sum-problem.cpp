c93633f44ce9dc545de060f9a58c4cf324e7da51
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-03-19 01:49:04 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,35 +9,37 @@ using namespace std;
 
 class Solution{   
 public:
-int dp[101][10001];
-
-   
- bool solve(int arr[],int n,int sum){
-     
-    if(sum==0)
-        return 1;
-    if(n==0 or sum<0)
-        return 0;
-    
-    if(dp[n][sum]!=-1)
-        return dp[n][sum];
-    
-    if(arr[n-1]<=sum){
-         dp[n][sum] = solve(arr,n-1,sum) or solve(arr,n-1,sum-arr[n-1]);
-    }
-    else
-         dp[n][sum] = solve(arr,n-1,sum);
+    bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        int n=arr.size();
+        int dp[n+1][sum+1];
+        memset(dp,0,sizeof(dp));
         
-        
-     return dp[n][sum];
- }
-
-    bool isSubsetSum(int n, int arr[], int sum){
-          
-            dp[n+1][sum+1];
-            memset(dp,-1,sizeof(dp));
-         return  solve(arr,n,sum);
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+       
+                if(i==0)
+                    dp[i][j]=0;
+                 if(j==0)
+                   dp[i][j]=1;
+                //   else if(dp[i][j]!=-1)
+                
+            }
             
+        }
+         for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                
+                    
+                 if(arr[i-1]<=j){
+                      dp[i][j]=dp[i-1][j] or dp[i-1][j-arr[i-1]];
+                  }
+                  else{
+                      dp[i][j]=dp[i-1][j];
+                  }
+}
+}
+        return dp[n][sum];
     }
 };
 
@@ -50,14 +52,14 @@ int main()
     {
         int N, sum;
         cin >> N;
-        int arr[N];
+        vector<int> arr(N);
         for(int i = 0; i < N; i++){
             cin >> arr[i];
         }
         cin >> sum;
         
         Solution ob;
-        cout << ob.isSubsetSum(N, arr, sum) << endl;
+        cout << ob.isSubsetSum(arr, sum) << endl;
     }
     return 0; 
 } 
