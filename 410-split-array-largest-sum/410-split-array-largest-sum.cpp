4683941484a80102fb9ac5bcf796e5a40f5e11ae
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-03-31 14:45:39 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (43.47%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    bool canSplit(int mid,vector<int>&nums,int m){
+        
+        int total=0;
+        int cnt=1;
+        for(auto it:nums){
+            total+=it;
+            if(total>mid){
+                total=it;
+                cnt++;
+            }
+            if(cnt>m)
+                    return false;
+        }
+        // cout<<"returning from can split function"<<cnt<<endl;
+        return true;
+    }
+    int splitArray(vector<int>& nums, int m) {
+     
+        int n=nums.size();
+        int left=INT_MIN,right,mid;
+        for(int i=0;i<n;i++){
+            
+            left=max(left,nums[i]);
+            right+=nums[i];
+        }
+   
+        while(left<right){
+            mid=(left+right)/2;
+            cout<<mid<<" "<<endl;
+            if(canSplit(mid,nums,m)){
+                right=mid;
+            }
+            else
+                left=mid+1;
+        }
+        return left;
+    }
+};
