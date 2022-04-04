7c123bd72dd21eedd8a797dad2d363d0df310619
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-04-04 16:52:20 +0530

    Added solution - LeetHub

---
+++ Previous number in one swap - GFG/previous-number-in-one-swap.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{   
+public:
+    string previousNumber(string s){
+        // code here 
+        int n=s.length();
+        int pos=-1,pos1;
+        
+        for(int i=n-2;i>=0;i--){
+            if(s[i]>s[i+1]){
+                pos=i;
+                break;
+            }
+        }
+        
+        if(pos==-1)
+            return "-1";
+        
+        for(int i=pos+1;i<n;i++){
+            
+            if(s[i]<s[pos] and s[i]!=s[i-1])
+                pos1=i;
+        }
+        swap(s[pos],s[pos1]);
+        if(s[0]=='0')
+            return "-1";
+            
+        return s;
+    }
+};
+
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.previousNumber(S) << endl;
+    }
+    return 0; 
+}   // } Driver Code Ends
