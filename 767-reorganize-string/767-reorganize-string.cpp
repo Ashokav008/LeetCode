e9c1991771a55722d87ec302af5956487d370d5d
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-04-25 16:05:58 +0530

    Time: 5 ms (30.54%), Space: 6.4 MB (36.21%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        
+        // cout<<to_char('a'+2)<<endl;
+        map<char,int>mp;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+            
+        }
+        priority_queue<pair<int,char>>pq;
+        for(auto it:mp){
+            // cout<<it.first<<" "<<it.second<<endl;
+            pq.push({it.second,it.first});
+        }
+        
+      if( pq.top().first>(n/2)+(n%2))
+            return "";
+     
+    
+        pair<int,char>pr={1,'#'};
+        string ans;
+        int i=0;
+       while(pq.size()>1)
+        {
+            auto t1=pq.top(); pq.pop();
+            auto t2=pq.top(); pq.pop();
+            
+            ans+=t1.second; ans+=t2.second;
+            t1.first-=1; t2.first-=1;
+            
+            if(t1.first>0){
+                pq.push(t1);
+            }
+            if(t2.first>0){
+                pq.push(t2);
+            }
+            
+        }
+        if(!pq.empty()){
+            if(pq.top().first>1)
+                return "";
+            else
+                ans+=pq.top().second;
+                
+        }
+    
+        return ans;
+
+        
+    }
+};
