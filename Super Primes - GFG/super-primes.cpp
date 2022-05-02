c959fd8962ddcaf512cd957a01183c04d2317a7f
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-05-02 08:54:43 +0530

    Added solution - LeetHub

---
+++ Super Primes - GFG/super-primes.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	void SieveOfEratosthenes(int n,unordered_set<int>&v)
+{
+    // Create a boolean array "prime[0..n]" and initialize
+    // all entries it as true. A value in prime[i] will
+    // finally be false if i is Not a prime, else true.
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+ 
+    for (int p = 2; p * p <= n; p++) {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true) {
+            // Update all multiples of p greater than or
+            // equal to the square of it numbers which are
+            // multiple of p and are less than p^2 are
+            // already been marked.
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+ 
+    // Print all prime numbers
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            v.insert(p);
+}
+	
+	int superPrimes(int n)
+	{
+	    // Your code goes here
+	    
+	    unordered_set<int>st;
+	    SieveOfEratosthenes(n,st);
+	    int ans=0;
+	    for(auto it:st){
+	        if(st.find(it+2)!=st.end())
+	            ans++;
+	    }
+	  
+	  return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		int n;
+   		cin >> n;
+   		Solution ob;
+   		cout << ob.superPrimes(n) << "\n";
+   	}
+
+    return 0;
+}
+  // } Driver Code Ends
