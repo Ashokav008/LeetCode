1e977b29fdcde6cda7908ea6764d483cb9243cbd
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-05-29 17:27:30 +0530

    Time: 165 ms (31.75%), Space: 23.8 MB (16.45%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxProduct(vector<string> str) {
+        
+   int ans=INT_MIN;
+        int n=str.size();
+        vector<vector<int>>v(n,vector<int>(26,0));
+      for(int i=0;i<n;i++){
+          for(int j=0;j<str[i].size();j++){
+              v[i][str[i][j]-'a']=1;
+              
+          }
+       
+      }
+    
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+              bool checked=true;
+                // cout<<str[i]<<"   "<<str[j]<<endl;
+                for(int k=0;k<26;k++){
+                if(  v[i][k]==1 and v[j][k]==1)
+                {
+                    // cout<<"Checked for "<<v[i][k]<<"  "<<v[j][k]<<endl;
+                    checked=false;
+                    break;
+                }
+                }
+                
+                    if(checked){
+                        int curr=str[i].size()* str[j].size();
+                        ans=max(ans,curr);
+                    }
+            }
+        
+        }
+        if(ans<0)
+                return 0;
+        return ans;
+    }
+};
