80867d13ee07ac62c7c3df491f5f71c23a2bd167
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-06-09 17:26:24 +0530

    Time: 168 ms, Memory: 66.1 MB - LeetHub

--- increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
+        int n=nums.size();
         
-        int a=INT_MAX,b=INT_MAX,n=nums.size();
-        
+        vector<int>left(n);
+        vector<int>right(n);
+        int mini=INT_MAX;
         for(int i=0;i<n;i++){
-            if(nums[i]<a)
-                a=nums[i];
-            else if(a<nums[i] and nums[i]<b)
-                b=nums[i];
-            else if(a<nums[i] and b<nums[i])
+            mini=min(nums[i],mini);
+            left[i]=mini;
+            // cout<<left[i]<<" ";
+        }
+      // cout<<endl<<"Turn for right array"<<endl;
+        int maxi=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            maxi=max(nums[i],maxi);
+            right[i]=maxi;
+            // cout<<right[i]<<" ";
+        }
+    
+        
+        // cout<<endl<<"  ";
+        for(int i=1;i<n-1;i++){
+            // cout<<nums[i]<<" ";
+            // cout<<left[i-1]<<"  "<<nums[i]<< " "<<right[i+1]<<endl;
+            if(nums[i]>left[i-1] and nums[i]<right[i+1] and left[i-1]<right[i+1])
                     return true;
         }
         return false;
-        
     }
 };
