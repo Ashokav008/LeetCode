1d5c42c48139c6d73cc4105354014762418631fb
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-06-11 12:58:00 +0530

    Time: 19 ms, Memory: 13.6 MB - LeetHub

---
+++ search-for-a-range/search-for-a-range.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    
+     int binaryS(vector<int>&nums,int target){
+        
+        int s=0,e=nums.size()-1;
+        
+        int mid;
+        int ans=-1;
+        while(s<=e){
+            mid=(s+e)/2;
+            
+             if(nums[mid]==target){
+                ans=mid;
+                 e=mid-1;
+            }
+           else  if(nums[mid]>=target)
+                e=mid-1;
+            else if(nums[mid]<target)
+                s=mid+1;
+            
+        }
+        return ans;
+    }
+    
+    
+    int binaryL(vector<int>&nums,int target){
+        
+        int s=0,e=nums.size()-1;
+        
+        int mid;
+        int ans=-1;
+        while(s<=e){
+            mid=(s+e)/2;
+            
+             if(nums[mid]==target){
+                ans=mid;
+                 s=mid+1;
+                 
+            }
+           else  if(nums[mid]>=target)
+                e=mid-1;
+            else if(nums[mid]<target)
+                s=mid+1;
+            
+        }
+        return ans;
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>ans;
+        int lst=binaryL(nums,target);
+        int strt= binaryS(nums,target);
+         ans.push_back(strt);
+        ans.push_back(lst);
+       
+        return ans;
+        
+    }
+};
