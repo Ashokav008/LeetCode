aa43a9e910af4260493616e5f5a08a7e9e3d118b
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-06-11 13:08:25 +0530

    Time: 49 ms, Memory: 19.1 MB - LeetHub

---
+++ merge-intervals/merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& in) {
+        
+        sort(in.begin(),in.end());
+        int n=in.size();
+        vector<vector<int>>ans;
+        ans.push_back(in[0]);
+        
+        for(int i=1;i<n;i++){
+    
+            if(ans.back()[1]>=in[i][0] and ans.back()[1]<=in[i][1]){
+                ans.back()[1]=in[i][1];
+            }
+            else if(ans.back()[1]<in[i][0])
+                    ans.push_back(in[i]);
+            
+        }
+        return ans;
+    }
+};
