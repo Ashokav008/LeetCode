0413dfd43a899259062f7a3a1394de0ea40202ee
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-06-16 13:26:49 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -9,19 +9,19 @@ using namespace std;
 class Solution
 {
     public:
-    
-    int lcs(int x, int y, string s1, string s2,vector<vector<int>>&dp){
-        if(x==0 or y==0)
-            return 0;
-        if(dp[x][y]!=-1)
-            return dp[x][y];
+    // this is memoiszation
+    // int lcs(int x, int y, string s1, string s2,vector<vector<int>>&dp){
+    //     if(x==0 or y==0)
+    //         return 0;
+    //     if(dp[x][y]!=-1)
+    //         return dp[x][y];
         
-        if(s1[x-1] == s2[y-1])
-            return dp[x][y]=1+lcs(x-1,y-1,s1,s2,dp);
+    //     if(s1[x-1] == s2[y-1])
+    //         return dp[x][y]=1+lcs(x-1,y-1,s1,s2,dp);
             
-        else
-            return dp[x][y]= max(lcs(x,y-1,s1,s2,dp),lcs(x-1,y,s1,s2,dp));
-    }
+    //     else
+    //         return dp[x][y]= max(lcs(x,y-1,s1,s2,dp),lcs(x-1,y,s1,s2,dp));
+    // }
     
     //Function to find the length of longest common subsequence in two strings.
     int lcs(int x, int y, string s1, string s2)
@@ -29,8 +29,34 @@ class Solution
         // your code here
         vector<vector<int>>dp(x+1,vector<int>(y+1,-1));
         
-        return lcs(x,y,s1,s2,dp);
+         for(int i=0;i<x+1;i++)
+        {
+            for(int j=0;j<y+1;j++){
+    
+            if(i==0 or j==0){
+                dp[i][j]=0;
+            }                
+                
+                
+            }
+            
+        }
         
+        
+        for(int i=1;i<x+1;i++)
+        {
+            for(int j=1;j<y+1;j++){
+    
+           if(s1[i-1]==s2[j-1]) 
+                dp[i][j]=1+dp[i-1][j-1];
+            
+            else
+                dp[i][j]=max(dp[i][j-1], dp[i-1][j]);
+            
+        }
+        }
+        // return lcs(x,y,s1,s2,dp);
+        return dp[x][y];
     }
 };
 
