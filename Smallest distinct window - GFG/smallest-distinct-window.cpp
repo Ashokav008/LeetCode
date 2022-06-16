e441e9a89288eabbfcb0230127a31fac0b5a14d8
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-06-16 12:34:03 +0530

    Added solution - LeetHub

---
+++ Smallest distinct window - GFG/smallest-distinct-window.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int findSubString(string str)
+    {
+        // Your code goes here   
+        map<char,int>mp;
+       int n=str.size();
+       int i=0,j=0;
+       int ans=INT_MAX,curr;
+       set<char>st;
+       for(int i=0;i<n;i++){
+           st.insert(str[i]);
+       }
+       int sz=st.size();
+        for(int i=0;i<n;i++){
+            
+            mp[str[i]]++;
+            
+            while(mp[str[j]]>1){
+                mp[str[j]]--;
+                j++;
+                
+                // cout<<str[j]<<"  "<<endl;
+            }
+            if(mp.size()==sz){
+                 curr=i-j;
+                ans=min(ans,curr);
+            }
+        }
+        return ans+1;
+    }
+};
+
+// { Driver Code Starts.
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.findSubString(str) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
