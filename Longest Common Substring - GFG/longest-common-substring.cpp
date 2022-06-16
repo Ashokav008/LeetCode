97ab9dfeb99b306cba05a581cb450569c4413ced
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-06-16 15:48:03 +0530

    Added solution - LeetHub

---
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // int maxi=INT_MIN;
+    
+    // int lcs(string s1 , string s2, int n,int m,vector<vector<int>>&dp){
+    //     if(n==0 or m==0)
+    //         return 0;
+            
+    //     if(dp[n][m]!=-1)
+    //     return dp[n][m];
+        
+    //     if(s1[n-1] == s2[m-1]){
+    //         cout<<1<<endl;
+    //         maxi=max(dp[n][m],maxi);
+    //       return  dp[n][m] = 1+lcs(s1,s2,n-1,m-1,dp);
+    //         // return dp[n][m];
+    //     }
+    //     else{
+    //     //   cout<<"Inside the "<<endl;
+    //         return dp[n][m]= 0;
+    //       }
+    // }
+    int longestCommonSubstr (string s1, string s2, int n, int m)
+    {   
+        // your code here
+        //ha lcs cha variatoin aahe
+       int  maxi=0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        //  lcs(s1,s2,n,m,dp);
+        
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                
+                if(i==0 or j==0)
+                    dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    maxi=max(maxi,dp[i][j]);
+                }
+                else
+                    dp[i][j]=0;
+                }
+        }
+        
+        return maxi;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+  // } Driver Code Ends
