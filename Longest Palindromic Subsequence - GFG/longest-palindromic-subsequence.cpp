a8d3ea6b7a8e72fec39946c80bec7b5eb03ea96b
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-06-17 20:18:57 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -32,7 +32,19 @@ class Solution{
         int n=a.size(),m=n;
         
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        lps(a,b,m,n,dp);
+        // lps(a,b,m,n,dp);
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                
+                if(i==0 or j==0)
+                    dp[i][j]=0;
+                else if(a[i-1]==b[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
         
         return dp[n][m];
     }
