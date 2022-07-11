8db41ba401344e00ef1af15fc05273e268fdb036
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-07-11 16:28:46 +0530

    Time: 7 ms (57.61%), Space: 12.1 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-            int index1 = -1, index2 = -1;
-    
-    for(int i = nums.size()-2;i>=0;i--) //traversing from second last index and finding break point
-    {
-        if(nums[i]<nums[i+1])
-        {
-            index1 = i;
-            break;
-            return;
-        }
-    }
-    // if break point doesn't exist
-    if(index1 == -1)
-    {
-        reverse(nums.begin(),nums.end());
-        return;
-    }
-    else // if break point exist
-    {
-        for(int i=nums.size()-1;i>=0;i--)  //traversing from last index
-        {
-            if(nums[i] > nums[index1])
-            {
-                index2 = i;
+        
+        int idx1=-1,idx2=-1,n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx1=i;
                 break;
-                return;
             }
         }
+        
+        if(idx1==-1){
+            reverse(nums.begin(),nums.end());
+                return;
+        }
+        
+        for(int i=n-1;i>=0 ;i--){
+            if(nums[i]>nums[idx1]){
+                idx2=i;
+                break;
+            }
+            
+        }
+                
+        swap(nums[idx1],nums[idx2]);
+        
+        reverse(nums.begin()+idx1+1,nums.end());
+        
+        return;
     }
-    
-    swap(nums[index1], nums[index2]);
-    
-    reverse(nums.begin()+index1+1,nums.end());
-    
-    return ;
-}
 };
