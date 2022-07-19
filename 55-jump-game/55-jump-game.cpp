d137ee2055018f47d7794bb07238f4898c648451
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-07-20 00:45:28 +0530

    Time: 1246 ms (8.14%), Space: 51.4 MB (5.85%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    bool solve(int i,int n,vector<int>&nums, vector<int>&dp){
+        
+        if(i>=n-1){
+            return true;
+        }
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int reach=i+nums[i];
+        for(int k=i+1;k<=reach;k++){
+            if(solve(k,n,nums,dp))
+                    return dp[i]=true;
+        }
+        
+        return dp[i]=false;
+    }
+    
+    bool canJump(vector<int>& nums) {
+        int i=0,n=nums.size();
+        vector<int>dp(n+1,-1);
+        return solve(i,n,nums,dp);
+        
+    }
+};
