a9b87a357e0a8f0c9b4b2db4773ba91f51f43cfd
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-07-22 18:49:19 +0530

    Time: 719 ms (17.58%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -3,7 +3,10 @@ BEGIN
     declare M INT;
     set M=n-1;
   RETURN (
-    select distinct(salary)  from employee order by salary desc limit M,1
+    # select distinct(salary)  from employee order by salary desc limit M,1
+      select distinct(e1.salary) from employee e1 where 
+      n-1= (select count(distinct salary) from employee e2 where e2.salary > e1.salary)
+      
       
   );
 END
