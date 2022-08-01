e78af43f59ab70e35cd6eb8b9a2301521fb48292
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-08-01 14:51:56 +0530

    Time: 4 ms (28.94%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,40 +1,12 @@
 class Solution {
 public:
-    
-    int ans=0;
-    
-    void dfs(int i,int j, int n,int m , vector<vector<int>>&vis){
-           
-        if(i<0 or j<0 or i>=n or j>=m or vis[i][j]==1)
-                return;
-        
-            
-        if(i==n-1 and j==m-1){
-            ans++;
-            return;
-        }
-        // cout<<i<<" "<<j<<endl;
-        
-        vis[i][j]=1;
-        dfs(i+1,j,n,m,vis);
-        dfs(i,j+1,n,m,vis);
-        vis[i][j]=0;
-        
-    }
-    
-    int uniquePaths(int n, int m) {
-        ans=0;
-        vector<vector<int>>vis(n+1,vector<int>(m+1,1));
-        int i=0,j=0;
-     // dfs(i,j,n,m,vis);
-        
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
+    int uniquePaths(int m, int n) {
+      vector<vector<int>>vis(m+1,vector<int>(n+1,1)) ;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 vis[i][j]=vis[i-1][j]+vis[i][j-1];
             }
         }
-        return vis[n-1][m-1];
-        
-        
+        return vis[m-1][n-1];
     }
 };
