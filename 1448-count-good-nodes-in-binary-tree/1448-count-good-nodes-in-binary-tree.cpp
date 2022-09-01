3c6c02065c85f7bde2824781577c3cf68eb7cb27
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-09-01 23:45:10 +0530

    Time: 279 ms (9.04%), Space: 86.4 MB (9.98%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -17,15 +17,13 @@ public:
                 return 0;
         int res=root->val>=ma?1:0;
         res+=solve(root->left,max(ma,root->val));
-        res+=solve(root->right, max(ma,root->val));
-        
+         res+=solve(root->right,max(ma,root->val));
         return res;
         
     }
     
     int goodNodes(TreeNode* root) {
-       
-        int ma=INT_MIN;
-        return solve(root,ma);
+            int ma=INT_MIN;
+            return solve(root,ma);    
     }
 };
