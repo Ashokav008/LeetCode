0b3548d4e2b87b6ccf786333b61e497ccd825673
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-09-09 23:30:36 +0530

    Time: 1534 ms (11.14%), Space: 125.2 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -15,15 +15,15 @@ public:
         sort(arr.begin(),arr.end(), comp);
         int ans=0;
         int n=arr.size();
-        int first=arr[0][0],second=arr[0][1];
-
-        int mtn=INT_MIN;
+    int mtn=INT_MIN;
         for(int i=n-1;i>=0;i--){
-            if(arr[i][1]<mtn)
-                ans++;
+            
+            if(arr[i][1]<mtn)ans++;
+            
             mtn=max(mtn,arr[i][1]);
-        
-    }
+            
+        }
+       
         return ans;
        
     }
