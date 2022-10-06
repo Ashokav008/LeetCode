330da8f41acac1e208647011fb69ac4bf73df844
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-10-06 19:15:16 +0530

    Time: 942 ms (5.00%), Space: 128.2 MB (70.01%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -9,21 +9,15 @@ public:
     void set(string key, string value, int timestamp)
     {
         mp[key].push_back({timestamp, value});
-        // Storage is a hasmap storing timestamp and value as vector of pairs
-        // against a timestamp
     }
     string get(string key, int timestamp)
     {
         if (mp[key].size() == 0)
             return "";
-        // If for that key no such pair is available then we cannot get
-        // any value
-        int n = mp[key].size();
-        // If there is vector of pairs then there will be size also
         int low = 0;
+        int n = mp[key].size();
         int high = n - 1;
         int mid = -1;
-        // We are using binary search to find the value pair <= timestamp
         while (low <= high)
         {
             mid = (low + high) / 2;
