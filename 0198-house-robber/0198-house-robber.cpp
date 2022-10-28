ae350e97bbf2b88b9f391c6decab5d60f3b832b7
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-10-28 13:35:27 +0530

    Time: 6 ms (17.37%), Space: 7.8 MB (25.72%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -17,8 +17,30 @@ public:
     }
     
     int rob(vector<int>& nums) {
-        // int n=nums.size();
-    vector<int>dp(nums.size(),-1);
-        return solve(0,nums,dp);
+        int n=nums.size();
+        vector<int>dp(nums.size(),-1);
+        
+        if(n==1)
+                return nums[0];
+        if(n==2)
+                return max(nums[0],nums[1]);
+        // return solve(0,nums,dp);
+        dp[0]=nums[0];
+        // dp[1]=nums[1];
+        
+        for(int i=1;i<n;i++){
+            int take,notTake;
+            if(i-2<0){
+               take=nums[i];
+            }else
+                    take=nums[i]+dp[i-2];
+            
+            notTake=dp[i-1];
+            
+            dp[i]=max(take,notTake);
+            
+            
+        }
+        return dp[n-1];
     }
 };
