de97f3e8029802a17218a33bf0ef14698ccd202a
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-10-29 19:10:35 +0530

    Time: 23 ms (44.06%), Space: 9.1 MB (83.36%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<string>ans;
+        int n=words.size();
+        vector<int>track(26,0),ff(26,0); 
+        
+        for(auto it:words[0])
+                ff[it-'a']++;
+        
+        for(int i=1;i<n;i++){
+            
+            for(auto it: words[i]){
+                track[it-'a']++;
+            }            
+            
+            for(int i=0;i<26;i++){
+                ff[i]=min(ff[i], track[i]);
+                track[i]=0;
+            }
+        }
+        
+        for(int i=0;i<26;i++){
+            
+            int cnt=ff[i];
+            
+            while(cnt--){
+                char ch=i+'a';
+                string str;
+                str=ch;
+                ans.push_back(str);
+            }
+        }
+        
+        return ans;
+    }
+};
