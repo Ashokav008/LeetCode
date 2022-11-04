e482cecf995fbcc11764c7683045251c6f1a6ea0
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-11-04 19:15:51 +0530

    Time: 135 ms (5.13%), Space: 7.9 MB (32.73%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int n=s.size();
+        int i=0,j=n-1;
+        unordered_map<char,int>mp{{'a',1},{'e',1},{'i',1},{'o',1},{'u',1},{'A',1},{'E',1},{'I',1},{'O',1},{'U',1}};
+        while(i<j){
+            
+            while(i<j and mp.find(s[i])==mp.end()){
+                        i++;
+                cout<<"First"<<endl;
+            }
+            
+            while(i<j and mp.find(s[j])==mp.end()){
+                        j--;
+                cout<<"second"<<endl;
+            }
+            
+            if(i<j)
+            {
+                swap(s[i],s[j]);
+                i++;j--;
+                
+            }
+            
+            
+        }
+        
+        return s;
+    }
+};
