10f86d777ff922bd9eedda2375dfa67a6eb4ed4b
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-11-08 12:09:16 +0530

    Time: 6 ms (48.12%), Space: 6.1 MB (92.00%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool solve(string &s){
+        
+        
+        
+        
        bool flg=true;
         for(int i=0;i<s.size()-1;i++){
                 if((s[i]-'a' == s[i+1]-'A') or ( s[i]-'A'==s[i+1]-'a')){
                         flg=false;
-                    cout<<"erasing   " <<i<<2<<endl;
                         s.erase(i,2);
-                    i-=2;
+                        i-=2;
+                    
                 }
             
         }
@@ -16,9 +20,6 @@ public:
     }
     
     string makeGood(string s) {
-       
-        
-        
         while(1){
             if(s.size()<2)
                     break;
