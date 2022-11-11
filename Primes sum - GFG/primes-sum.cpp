427bd1c274ab4db643a0f3ea6052a54fd76b94e3
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-11-11 17:01:28 +0530

    Added solution - LeetHub

--- Primes sum - GFG/primes-sum.cpp
+++ Primes sum - GFG/primes-sum.cpp
@@ -1,48 +1,49 @@
-// { Driver Code Starts
-
+//{ Driver Code Starts
 // Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 // User function Template for C++
 class Solution {
-  public:
-  
-  
- bool isPrime(int n)
+public:
+bool isPrime(int n)
 {
+    // Corner cases
     if (n <= 1)
         return false;
+    if (n <= 3)
+        return true;
  
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+ 
+    // Using concept of prime number
+    // can be represented in form of
+    // (6*n + 1) or(6*n - 1) hence
+    // we have to 
+    // prime number would always be 1 less orgo for every multiple of 6 and 1 more than
+    // the multiple of 6.
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
             return false;
+ 
     return true;
 }
- 
-  
+
     string isSumOfTwo(int n){
-        
-     
-     set<int>st;
-     for(int i=2;i<n;i++){
-         if(isPrime(i)){
-            //  cout<<i<<endl;
-            st.insert(i);
-         }
-         }
-     
-    for(auto it:st){
-        if(st.find(n-it)!=st.end())
-            return "Yes";
-    }     
-    return "No";
-        
+        int i=2,j=n-2;
+        while(i<=j){
+            if(isPrime(i) and isPrime(j))
+                return "Yes";
+                i++;
+                j--;
+        }
+        return "No";
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
@@ -54,4 +55,5 @@ int main() {
     }
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
