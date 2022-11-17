80b9bc26cb92303b96893fbd50fa64d3d98601ae
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2022-11-18 01:39:33 +0530

    Time: 515 ms (72.82%), Space: 173.4 MB (23.30%) - LeetHub

--- 1977-number-of-ways-to-separate-numbers/1977-number-of-ways-to-separate-numbers.cpp
+++ 1977-number-of-ways-to-separate-numbers/1977-number-of-ways-to-separate-numbers.cpp
@@ -1,29 +1,99 @@
+const int mod=1000000007;
+
+int go(int pos,string &prev,string &num,map<pair<int,string>,int> &dp){
+    
+    
+    if(dp.count(make_pair(pos,prev)))
+        return dp[make_pair(pos,prev)];
+    
+    int prev_size=prev.size();
+    int n=num.size();
+    
+    if(pos>=n)
+        return 1;
+    
+    if(num[pos]=='0')
+        return 0;
+    
+    int ans=0;
+    string newString="";
+    for(int next_pos=pos;next_pos<n;next_pos++){
+            newString+=num[next_pos];
+            if(newString.size()<prev_size)
+                continue;
+             if(newString.size()==prev.size()){
+                 if(newString>=prev){
+                     ans+=go(next_pos+1,newString,num,dp);
+                     ans%=mod;
+                 }
+             }
+        else {
+            ans+=go(next_pos+1,newString,num,dp);
+            ans%=mod;
+        }
+    }
+    
+    return dp[make_pair(pos,prev)]=ans;
+}
 class Solution {
 public:
-    using vl = vector<long>;
-    using vll = vector<vl>;
-    int mod = 1e9 + 7;
-    int numberOfCombinations(string &num) {
-        if (num[0] == '0') return 0;
-        int n = num.size();
-        vll dp(n, vl(n + 1, 0)), same(n + 1, vl(n + 1));
-        for (int i = n - 1; i >= 0; i--)
-            for (int j = i + 1; j < n; j++)
-                same[i][j] = num[i] == num[j] ? same[i + 1][j + 1] + 1 : 
-                             num[i] > num[j] ? 0 :
-                             INT_MAX;
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= i; j++) {
-                int s = i - j + 1;
-                if (num[s] == '0') dp[i][j] = dp[i][j - 1];
-                else if (s >= j && same[s - j][s] >= j) 
-                    dp[i][j] = (dp[i][j - 1] + dp[s - 1][j]) % mod;
-                else
-                    dp[i][j] = (dp[i][j - 1] + dp[s - 1][min(j - 1, s)]) % mod;
+    int numberOfCombinations(string nums) {
+        int n=nums.size();
+        int ans=0;
+        int dp[n][n+1];
+        memset(dp,0,sizeof(dp));
+        string prev="";
+        /////int lcp[n][n];
+        vector<vector<int>>lcp(n,vector<int>(n,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+                if(nums[i]==nums[j]){
+                    lcp[i][j]=1;
+                    if(j+1<n)
+                    lcp[i][j]+=lcp[i+1][j+1];
+                }
             }
-            dp[i][i + 1] = (dp[i][i] + 1) % mod;
         }
-        return dp.back().back();
+        int pref[n][1+n];
+        memset(pref,0,sizeof(pref));
+        
+        for(int i=0;i<n;i++){
+            for(int j=i;j>=0;j--){
+                int len=i-j+1;
+                
+                if(nums[j]=='0')
+                    continue;
+                 //   j ..... i
+                // ...... (j-1) j..........i 
+               int standing=j-1;
+                // dp[sta][k] for all k>=1 and k<len 
+                if(standing>=0)
+                dp[i][len]+=pref[standing][len-1];
+                else 
+                    dp[i][len]=1;
+                
+                dp[i][len]%=mod;
+                int where=standing-len+1;
+                if(where>=0){
+                    if(lcp[where][j]>=len ||  nums[where+lcp[where][j]]<nums[j+lcp[where][j]]){
+                        if(standing>=0)
+                        {dp[i][len]+=dp[standing][len];
+                        dp[i][len]%=mod;}
+                    }
+                }
+                
+            }
+            
+            for(int len=1;len<=n;len++){
+                pref[i][len]=dp[i][len];
+                pref[i][len]+=pref[i][len-1];
+                pref[i][len]%=mod;
+            }
+        }
+        
+        return pref[n-1][n];
+        //return go(0,prev,num,dp);
+
     }
 };
-
