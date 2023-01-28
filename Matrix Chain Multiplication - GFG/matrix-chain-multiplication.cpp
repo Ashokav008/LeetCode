41706ce55273cbdb0887e0fefb583d7893b45a33
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2023-01-28 17:26:22 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -25,9 +25,25 @@ public:
     }
 
     int matrixMultiplication(int n, int arr[])
-    {   vector<vector<int>>dp(n,vector<int>(n,-1));
+    {   
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
         // code here
-        return solve(1,n-1, arr,dp);
+        // return solve(1,n-1, arr,dp);
+        int dp[n][n];
+        for(int i=1;i<n;i++)   dp[i][i]=0;
+        
+        for(int i=n-1;i>=1 ;i--){
+            for(int j=i+1;j<n;j++){
+                    int mini=1e9;
+                    for(int k=i;k<j;k++){
+                        int step=arr[i-1]* arr[k] * arr[j] + dp[i][k] + dp[k+1][j];
+                        mini=min(mini, step);
+                    }
+                     dp[i][j]= mini;
+                
+            }
+        }
+        return dp[1][n-1];
         
     }
 };
