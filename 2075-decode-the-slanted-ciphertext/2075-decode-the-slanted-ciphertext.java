26eefb00f64c86946c2b62bdbfd6c8f3c64a9815
Ashok Vavare <74716546+Ashokav008@users.noreply.github.com>
2026-04-05 02:29:23 +0530

    Time: 11 ms (99.38%), Space: 56.5 MB (98.15%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -1,14 +1,11 @@
 class Solution {
     public String decodeCiphertext(String str , int rows) {
         
-        int i=0,n=(str.length())/rows;
-        int j=0,jgbl=1;
+        int i=0,n=(str.length())/rows,j=0,jgbl=0;
+      
         StringBuilder ans=new StringBuilder();
        
         while(j<n){
-            // if(i==rows && j==n-1)
-            //     break;
-
             int strIdx=(i*n+j);
               ans.append(str.charAt(strIdx));
             //  System.out.println("i="+i+"j="+j+"idx="+strIdx+" "+   ans);
@@ -17,12 +14,10 @@ class Solution {
 
             if(i==rows){
                 i=0;
-                j=jgbl;
                 jgbl++;
+                j=jgbl;
+                
             }
-            // if(jgbl==n+1)
-            //     break;
-
         }
     
         return ans.toString().stripTrailing();
